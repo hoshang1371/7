9383fb1501da446a8c96b2cb821eb0c21c8f3cbd
Hojjat <hoshang.1371@yahoo.com>
2020-01-11 12:11:17 +0330

    5 commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
+from . import views
 
+app_name = 'catalog'
 urlpatterns = [
-
+    #url(r'^$', views.index , name='index'),
+    url(r'^$', views.index , name='index'),
+    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
